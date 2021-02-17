c811a1422fcb972fb4c894f8575bfe5c906d2dc6
Weighed <wade.loccisano@gmail.com>
2021-02-17 04:49:15 -0500

    Add Bootstrap styles

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
 
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 101);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'This is the description',
+            price
 
         })
         await camp.save();
